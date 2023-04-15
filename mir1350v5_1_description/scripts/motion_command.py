c77ab6ebc545d2fbe8bd8ecd2ecf1bb1c05ee459
ruk-shan <ruk.shan@outlook.com>
2023-04-16 00:52:32 +0800

    first commit

---
+++ mir1350v5_1_description/scripts/motion_command.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+import rospy
+from std_msgs.msg import Float64
+import math
+
+def talker():
+    pub = rospy.Publisher('/rrbot/joint1_position_controller/command', Float64, queue_size=10)
+    rospy.init_node('talker', anonymous=True)
+    rate = rospy.Rate(10) # 10hz
+    
+    while not rospy.is_shutdown():
+        hello_str = "hello world %s" % rospy.get_time()
+        position = math.pi/2
+        rospy.loginfo(position)
+        pub.publish(position)
+        rate.sleep()
+
+if __name__ == '__main__':
+    try:
+        talker()
+    except rospy.ROSInterruptException:
+        pass
